a7228e6447514262315b99f862e24cc56dfaa426
artzaarock <73689671+artzaarock@users.noreply.github.com>
2020-10-30 15:49:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <html>
     <head>
+        <meta charset="UTF-8">
         <title>Hello world</title>
     <head>
     <body>
-        <h1>My First Heading</h1>
-        <p>My First Paragraph</p>
+        <h1>Name-Tummanun mongkol</h1>
+        <p>Nickname-Art</p>
         <img src="cat.jpg">
+        <img src="cat2.jpg">
         <br>
         <a href="http://ww.it.kmitl.ac.th">This is a Link</a>
         
-        <h2>The unordered list</h2>
+        <h2>HAHAHA</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li>หมอ: สวัสดี คุณจะมาปรึกษาปัญหาเรื่องตาใช่ไหม</li>
+            <li>คนไข้: คุณหมอรู้ได้ไงครับ</li>
+            <li>หมอ: เพราะคุณเข้ามาทางหน้าต่างน่ะสิ</li>
+            
         </ul>
     
     </body>
 </head>
+
